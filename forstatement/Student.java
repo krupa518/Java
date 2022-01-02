bc40e1a96973f0fa861fdc613adef1214ff69a4d
krupa18 <krupavathisayina26gmail.com>
2022-01-02 21:54:09 +0530

    add file

---
+++ forstatement/Student.java
@@ -0,0 +1,19 @@
+/*
+package com.training.forstatement;
+
+
+
+import com.sun.xml.internal.ws.developer.MemberSubmissionAddressing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    public static void main(String... args) {
+        College1 college1 = new College1();
+        List<College> collegeList = new CollegeInfo();
+        college1.validationCollege(collegeList);
+    }
+
+}
+*/
